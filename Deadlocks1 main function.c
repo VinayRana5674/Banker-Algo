6ab2d83960137b3172c87e7a3f26d82515476cf2
VinayRana5674 <38176185+VinayRana5674@users.noreply.github.com>
2018-04-08 13:08:16 +0530

    Update and rename Deadlocks1 main function to Deadlocks1 main function.c

--- Deadlocks1 main function.c
+++ Deadlocks1 main function.c
@@ -3,26 +3,27 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<semaphore.h>
-pthread_t a[2];//this could be n number of array for n number of process
 void * thread();
-int c=0;// count initialization
+pthread_t p[2];//this could be n number of array for n number of process
 pthread_mutex_t l1;// for test i have taken only two mutex initially
 pthread_mutex_t l2;
+int count=0;// count initialization
 int main()
 {
 	
 	pthread_mutex_init(&l1,NULL);// for initializing it's in unlock state
+	pthread_mutex_init(&12,NULL);
 	int i,res;
 	void * msg;
 	for(i=0;i<2;i++) // create 2 threads 
 	{
-		res=pthread_create(&a[i],NULL,thread,NULL);
+		res=pthread_create(&p[i],NULL,thread,NULL);
 		
 	} 
 	for(i=0;i<2;i++)//join those threads
 	{
-		res=pthread_join(a[i],&msg);
+		res=pthread_join(p[i],&msg);
 	}
-	printf("count is %d\n",c);// count at last
+	printf("count is %d\n",count);// count at last
 	return 0;
 }
